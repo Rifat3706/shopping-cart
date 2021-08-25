505e387e60bf7db457064537952a0b2cb7d12c37
Rifat3706 <rokibulhasnatrifat3706@gmail.com>
2021-08-26 04:00:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+function updateProductNumber(product, isIncreasing, price) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
+}
+// handle case increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', true, 1219);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', false, 1219);
+})
+// handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    // caseInput.value = parseInt(caseNumber) + 1;
+    updateProductNumber('case', true, 59);
+})
+document.getElementById('case-minus').addEventListener('click', function () {
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    // caseInput.value = parseInt(caseNumber) - 1;
+    updateProductNumber('case', false, 59);
+})
+// calculate total
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal() {
+    // const phoneInput = document.getElementById('phone-number');
+    // const phoneNumber = parseInt(phoneInput.value);
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    // tax amount
+    const taxAmount = subTotal / 10;
+    // total amount
+    const totalAmount = subTotal + taxAmount;
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = taxAmount;
+    document.getElementById('total-amount').innerText = totalAmount;
+}
